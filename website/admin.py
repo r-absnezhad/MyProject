cfb8c69544ad6867bc65376343c774752999fa11
r-absnezhad <167188241+r-absnezhad@users.noreply.github.com>
2024-08-10 15:10:40 +0330

    completing Contact
    + creating Contact Model
    + creating ContactAdmin
    + registering Contact

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from website.models import Contact
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_filter = ('email', )
+    ordering = ['-created_date']
+    search_fields = ['name', 'email','message']
+    list_display = ['name', 'email','created_date']
+    
+
+
+admin.site.register(Contact, ContactAdmin)  
--- website/models.py
+++ website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=256)
+    email = models.EmailField()
+    subject = models.CharField(max_length=256)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_date',)
+
+    def __str__(self):
+        return "{}. {}".format(self.name, self.email)
+
+ 
